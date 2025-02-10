8e1d96821b98041ad28511a416885c24e2e7ef2e
Roman <lototskiyroman@gmail.com>
2025-02-10 16:02:57 +0200

    backroads-app

---
+++ src/components/Tours.jsx
@@ -0,0 +1,44 @@
+import Title from "./Title";
+import { tours } from "../data";
+
+const Tours = () => {
+  return (
+    <section className="section" id="tours">
+      <Title title="featured" subTitle="tours" />
+      <div className="section-center featured-center">
+        {tours.map((tour) => {
+          return (
+            <article className="tour-card" key={tour.id}>
+              <div className="tour-img-container">
+                <img src={tour.image} className="tour-img" alt="" />
+                <p className="tour-date">{tour.date}</p>
+              </div>
+              <div className="tour-info">
+                <div className="tour-title">
+                  <h4>{tour.title}</h4>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Cumque vitae tempore voluptatum maxime reprehenderit eum quod
+                  exercitationem fugit, qui corporis.
+                </p>
+                <div className="tour-footer">
+                  <p>
+                    <span>
+                      <i className="fas fa-map"></i>
+                    </span>{" "}
+                    {tour.location}
+                  </p>
+                  <p>{tour.duration}</p>
+                  <p>from ${tour.cost}</p>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
